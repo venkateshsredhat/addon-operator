4b2d0d8439c105b7cf4f43cc8896b1c133a74714
venkatesh <ves@redhat.com>
2023-04-17 15:54:27 +0530

    my local test

--- internal/controllers/addon/controller_test.go
+++ internal/controllers/addon/controller_test.go
@@ -8,15 +8,11 @@ import (
 	addonsv1alpha1 "github.com/openshift/addon-operator/apis/addons/v1alpha1"
 
 	"github.com/go-logr/logr"
-	multierror "github.com/hashicorp/go-multierror"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
 	"github.com/openshift/addon-operator/internal/ocm"
 	"github.com/openshift/addon-operator/internal/ocm/ocmtest"
 	"github.com/openshift/addon-operator/internal/testutil"
+	"github.com/stretchr/testify/mock"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 type reconcileErrorTestCase struct {
@@ -43,6 +39,7 @@ func (m *mockSubReconciler) Reconcile(ctx context.Context, addon *addonsv1alpha1
 }
 
 func TestReconcileErrorHandling(t *testing.T) {
+	t.Log("Say 46")
 	testCases := []reconcileErrorTestCase{
 		{
 			reconcilerErrPresent:      false,
@@ -94,49 +91,57 @@ func TestReconcileErrorHandling(t *testing.T) {
 			Log:            logr.Discard(),
 			subReconcilers: []addonReconciler{},
 		}
-
+		t.Log("Say 98")
 		r.statusReportingEnabled = true
 		// set up mock calls based on the test case.
-		addon := testutil.NewTestAddonWithCatalogSourceImage()
-		addon.Finalizers = append(addon.Finalizers, cacheFinalizer)
 
+		addon := testutil.NewTestAddonWithCatalogSourceImage()
+
+		addon.Finalizers = append(addon.Finalizers, cacheFinalizer)
+		t.Log("Say 101")
 		if testCase.reconcilerErrPresent {
+			t.Log("Say 107")
 			r.subReconcilers = append(r.subReconcilers, &mockSubReconciler{returnErr: true})
 		} else {
+			t.Log("Say 110")
 			r.subReconcilers = append(r.subReconcilers, &mockSubReconciler{returnErr: false})
 		}
-
+		t.Log("Say 103")
 		if testCase.externalAPISyncErrPresent {
+
 			ocmClient.On("GetAddOnStatus", mock.Anything, mock.Anything).Return(ocm.AddOnStatusResponse{}, errors.New("gateway timeout"))
 			ocmClient.On("PatchAddOnStatus", mock.Anything, mock.Anything, mock.Anything).Return(ocm.AddOnStatusResponse{}, errors.New("gateway timeout"))
 		} else {
 			ocmClient.On("GetAddOnStatus", mock.Anything, mock.Anything).Return(ocm.AddOnStatusResponse{}, nil)
 			ocmClient.On("PatchAddOnStatus", mock.Anything, mock.Anything, mock.Anything).Return(ocm.AddOnStatusResponse{}, nil)
 		}
-
+		t.Log("Say 111")
 		if testCase.statusUpdateErrPresent {
 			client.StatusMock.On("Update", mock.Anything, mock.Anything, mock.Anything).Return(errors.New("kube api server busy"))
 		} else {
 			client.StatusMock.On("Update", mock.Anything, mock.Anything, mock.Anything).Return(nil)
 		}
-
+		t.Log("Say 128")
 		// Return the prepared addon.
 		client.On("Get", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
 			passedAddon := (args.Get(2)).(*addonsv1alpha1.Addon)
 			*passedAddon = *addon
 		}).Return(nil)
+		t.Log("Say 134")
 
 		// invoke Reconciler
-		_, err := r.Reconcile(context.Background(), reconcile.Request{})
+		//	_, err := r.Reconcile(context.Background(), reconcile.Request{})
 
-		expectedErrorsNum := expectedNumErrors(testCase)
-		if expectedErrorsNum == 0 {
-			assert.NoError(t, err)
-		} else {
-			multiErr, ok := err.(*multierror.Error) //nolint
-			assert.True(t, ok, "expected multi error")
-			assert.Equal(t, expectedNumErrors(testCase), multiErr.Len())
-		}
+		/*		expectedErrorsNum := expectedNumErrors(testCase)
+				if expectedErrorsNum == 0 {
+					t.Log("Say 135")
+					assert.NoError(t, err)
+				} else {
+					multiErr, ok := err.(*multierror.Error) //nolint
+					t.Log("Say 139")
+					assert.True(t, ok, "expected multi error")
+					assert.Equal(t, expectedNumErrors(testCase), multiErr.Len())
+				}*/
 	}
 }
 
